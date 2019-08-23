65afe78d710bffdc041e4b8ca04206587447750f
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2019-08-23 13:59:43 +0700

    อ่าน ip address มานับจำนวนครั้งได้

--- log_parser.py
+++ log_parser.py
@@ -1,11 +1,23 @@
+import re
+from collections import Counter
+
+
 def log_reader(logfile):
     """
     อ่านไฟล์จากชื่อที่ส่งมา แล้ว print เนื้อหาออกไป
     :param logfile: file path + file name
     """
+    re_ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
     with open(logfile) as f:
         log = f.read()
-    print(log)
+        ip_list = re.findall(re_ip_pattern, log)
+        ip_count = Counter(ip_list)
+
+        for k, v in ip_count.items():
+            print('IP Address: ' + k + ' occures ==> ' + str(v) + ' times.')
+
+        # print(log)
 
 
 if __name__ == '__main__':
