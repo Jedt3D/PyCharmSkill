b794b79f823c60f251ae38f97cfd21b4613ecec5
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2019-08-23 16:24:45 +0700

    สร้าง sqlite3 connection ได้ + select ได้

--- log_parser.py
+++ log_parser.py
@@ -1,4 +1,5 @@
 import re
+import sqlite3
 from collections import Counter
 
 re_ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -37,14 +38,43 @@ def create_sql_line_by_line(logfile):
             ip_address = re.findall(re_ip_pattern, line)
             access_date_time = re.findall(re_datetime_pattern, line)
 
-            print('insert into access values (`{ip}`, `{dt}`)'.format(ip=ip_address[0], dt=access_date_time[0]))
+            print('insert into access_log values (`{ip}`, `{dt}`)'.format(ip=ip_address[0], dt=access_date_time[0]))
+
+
+def create_connection_db(dbname):
+    try:
+        conn = sqlite3.connect(dbname)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+def show_total_records(conn):
+    sql = 'select count(id) from access_log'
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchone()
+
+    return str(rows[0])
 
 
 if __name__ == '__main__':
     # log_reader('access.log')
-
     # read and print line by line
     # read_datetime_line_by_line('access.log')
 
     # convert log to sql insert
-    create_sql_line_by_line('access.log')
+    # create_sql_line_by_line('access.log')
+
+    # create connection
+    dbname = '/Users/worajedt/PycharmProjects/pycharmskill/log.sqlite'
+    conn = create_connection_db(dbname)
+    with conn:
+        print('Current records BEFORE insert = ' + show_total_records(conn))
+
+        # insert records
+        # insert_log_to_table(conn)
+
+        print('Current records AFTER insert = ' + show_total_records(conn))
